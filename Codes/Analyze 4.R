0bc3708a0d43931777b6d6e803a4ec3d84e9c7c9
Andhika Nurrafli Putra <andhikanurrafli@gmail.com>
2024-11-11 10:53:15 +0700

    Update Analyze 4.R

--- Codes/Analyze 4.R
+++ Codes/Analyze 4.R
@@ -22,4 +22,24 @@ ggplot(membership_daily_dist, aes(x = Day, y = avg_dist, fill = membership_type)
   theme_minimal()+
   theme(axis.text.x = element_text(angle = 35, hjust = 1))+
   scale_fill_manual(values = c("member" = "skyblue", "casual" = "orange"))
-  
+
+# mengubah perintah visualisasi menjadi lebih simpel
+
+average_daily_ride_viz <- ggplot(membership_daily_dist, aes(x = Day, y = avg_dist, fill = membership_type))+
+  geom_bar(stat = "identity", position = "dodge", show.legend = TRUE)+
+  labs(title = "Average Daily Ride by Each Membership Type",
+       x = "Day",
+       y = "Average Distance (KM)" ) +
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 35, hjust = 1))+
+  scale_fill_manual(values = c("member" = "skyblue", "casual" = "orange"))
+
+
+ride_count_viz <- ggplot(membership_daily_ride, aes(x = Day, y = ride_count, fill = membership_type))+
+  geom_bar(stat = "identity", position = "dodge", show.legend = TRUE) +
+  labs(title = "Daily Ride by Membership Type",
+       x = "Day",
+       y = "Count") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 35, hjust = 1))+
+  scale_fill_manual(values = c("member" = "skyblue", "casual" = "orange"))
